548f956957c3cd33437f7f85e941cde351e055a5
oGalhardo <juniorgalhardo03@gmail.com>
2024-02-07 11:39:57 -0300

    ✨ Carousel for rh_divulga

--- src/components/Carousel/CarouselForCardsTitles/CarouselTitles.vue
+++ src/components/Carousel/CarouselForCardsTitles/CarouselTitles.vue
@@ -1,63 +1,117 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
-    <q-card class="card-carousel justify-center">
-      <q-carousel v-model="slide" class="card-carousel">
-        <q-carousel-slide name="style" class="slide justify-center">
-          <div class="row justify-center">
-            <q-card
-              v-for="(card, index) in cards"
-              :key="card.id"
-              class="my-card col-4 q-my-md text-center"
-            >
-              <q-card-section class="bg-indigo-8">
-                <div class="title-card text-h6 text-white">
-                  {{ card.title }}
-                </div>
-              </q-card-section>
+  <div class="carousel-slide q-pa-md">
+    <q-card
+      v-for="(card, index) in cards"
+      :key="card.id"
+      class="my-card text-center"
+    >
+      <q-card-section class="bg-indigo-8">
+        <div class="title-card text-h6 text-white">
+          {{ card.title }}
+        </div>
+      </q-card-section>
 
-              <q-card-actions vertical>
-                <div class="text-h6">
-                  <q-icon
-                    name="event"
-                    class="ico"
-                    size="2em"
-                    color="indigo-8"
-                  />
-                  {{ card.author }}
-                </div>
-              </q-card-actions>
-            </q-card>
-          </div>
-        </q-carousel-slide>
-      </q-carousel>
+      <q-card-actions class="q-pa-xl" vertical>
+        <div class="text-h6">
+          <q-icon name="event" class="ico" size="2em" color="indigo-8" />
+          {{ card.author }}
+        </div>
+      </q-card-actions>
+      <q-card-actions vertical>
+        <div class="text-lowercase see-more">
+          <q-btn color="indigo-8" no-caps>
+            <div>Veja mais</div>
+          </q-btn>
+        </div>
+      </q-card-actions>
     </q-card>
+    <div class="pagination">
+      <q-btn
+        class="btn-direction"
+        unelevated
+        no-caps
+        label="Anterior"
+        size="19px"
+      />
+      <q-badge
+        v-for="index in 10"
+        :key="index"
+        @click="changeColor(index)"
+        :color="buttonColors[index - 1]"
+        class="btns-pag text-indigo-8"
+        :class="{
+          'text-white':
+            activeButtonIndex === null || activeButtonIndex === index,
+        }"
+        unelevated
+        ><span :class="{ 'text-white': activeButtonIndex === index }">{{
+          index
+        }}</span></q-badge
+      >
+
+      <q-btn
+        class="btn-direction"
+        unelevated
+        no-caps
+        label="Próximo"
+        size="19px"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { cards } from "./lib";
 
-const slide = ref("style");
-const current = ref(3);
+const buttonColors = ref<string[]>(Array(10).fill("transparent"));
+const activeButtonIndex = ref<number | null>(null);
 
-const cards = ref([
-  { id: 1, title: "Integração 1", author: "John Doe" },
-  { id: 2, title: "Integração 2", author: "Jane Doe" },
-  { id: 3, title: "Integração 3", author: "Bob Smith" },
-  { id: 4, title: "Integração 4", author: "Alice Johnson" },
-  { id: 5, title: "Integração 5", author: "Eve Johnson" },
-  // Add more cards as needed
-]);
+const changeColor = (index: number) => {
+  if (activeButtonIndex.value !== null) {
+    buttonColors.value[activeButtonIndex.value - 1] = "transparent";
+  }
+
+  buttonColors.value[index - 1] = "green-8";
+  activeButtonIndex.value = index;
+};
 </script>
 
 <style scoped>
+.btn-direction {
+  padding: 10px;
+  color: gray;
+}
+.btns-pag {
+  height: 20px;
+  cursor: pointer;
+  font-size: large;
+}
+.pagination {
+  grid-column: 4;
+  position: relative;
+}
+.carousel-slide {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+  grid-auto-rows: 230px 30;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: -20px;
+  margin-bottom: 3%;
+}
 .my-card {
   border-radius: 20px;
-  overflow: hidden;
-}
-.card-carousel {
 }
 .title-card {
   border-radius: 20px;
+}
+.see-more {
+  left: 35%;
+  position: relative;
+  bottom: 30px;
+  border-radius: 30px;
 }
 </style>
