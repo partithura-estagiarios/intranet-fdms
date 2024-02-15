b6c32408b2543c6f67103f0704fbe1aa2643f017
oGalhardo <juniorgalhardo03@gmail.com>
2024-02-15 14:58:27 -0300

    ✨ Criacão da Page Home e feita a rota para ela através do login

---
+++ src/components/Pages/Home/PHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-img
+    src="/INTRANET_BACKGROUND_ FUNDIMISA.png"
+    class="absolute opacity fixed-full"
+  />
+  <BarUp />
+  <Image_Rh img="Welcome_Intranet.png" />
+  <Separator texto="SISTEMAS" large="300px" class="overlap" />
+  <Item />
+</template>
+<style scoped>
+.overlap {
+  z-index: 1000px;
+  bottom: 4%;
+}
+</style>
