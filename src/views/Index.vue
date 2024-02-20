5c3870f8f824406bc9fa7521c26778d9d65803d1
oGalhardo <juniorgalhardo03@gmail.com>
2024-02-20 08:11:37 -0300

    🐛 Tela de login apresentando header arrumado

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,1 +1,9 @@
-<template><Login /></template>
+<template></template>
+
+<script setup lang="ts">
+import { router } from "../modules";
+
+onMounted(() => {
+  router.push("/login");
+});
+</script>
