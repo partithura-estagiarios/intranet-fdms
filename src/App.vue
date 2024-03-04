70724495e9c422f68c8c0ae87acdc743ea29ef81
oGalhardo <juniorgalhardo03@gmail.com>
2024-03-04 08:52:04 -0300

    🐛 O erro era por conta que o useRoute não estava sendo iniciado antes, arrumei e agora está ok

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <BarUp v-if="showTabHeader" />
-  <BackgroundIntranet v-if="showTabHeader" />
-  <RouterView />
+  <div>
+    <BarUp v-if="showTabHeader" />
+    <BackgroundIntranet v-if="showTabHeader" />
+    <RouterView />
+  </div>
 </template>
+
 <script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+const currentRoute = useRoute();
+
 const exceptionRoutes = ["/login"];
+
 const showTabHeader = computed(() => {
-  return !exceptionRoutes.some((route) =>
-    window.location.pathname.includes(route),
-  );
+  return !exceptionRoutes.some((route) => currentRoute.path.includes(route));
 });
 </script>
