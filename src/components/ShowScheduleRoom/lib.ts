180a9f3710e75e9c6dee4120b4c14664477e908d
oGalhardo <juniorgalhardo03@gmail.com>
2024-04-10 10:56:30 -0300

    🐛 Exibindo ícones dos materiais extras e arrumando o background-color de mês

--- src/components/ShowScheduleRoom/lib.ts
+++ src/components/ShowScheduleRoom/lib.ts
@@ -1,6 +1,33 @@
 import { DateTime } from "luxon";
-export function getHoursMinutes(event: string) {
-  const dateTime = DateTime.fromISO(event);
-  const formattedTime = dateTime.toFormat("HH:mm");
-  return formattedTime;
+import { SuPMaterials } from "../../entities/supportMaterials";
+export function formatDateTime(dateTime: Date) {
+  return DateTime.fromJSDate(dateTime).toFormat("HH:mm");
+}
+enum ValidKeys {
+  projector = "projector",
+  water = "water",
+  flipSharp = "flipSharp",
+  equipamentSong = "equipamentSong",
+  coffee = "coffee",
+  computer = "computer",
+}
+
+export function renameKeys(materialsSup: SuPMaterials) {
+  const translations: Record<ValidKeys, string> = {
+    projector: "videocam",
+    water: "water_drop",
+    flipSharp: "filter_frames",
+    equipamentSong: "surround_sound",
+    coffee: "coffee",
+    computer: "computer",
+  };
+
+  Object.keys(materialsSup).forEach((key) => {
+    const validKey = key as ValidKeys;
+    if (translations[validKey]) {
+      materialsSup[validKey] = translations[validKey];
+    }
+  });
+
+  return materialsSup;
 }
