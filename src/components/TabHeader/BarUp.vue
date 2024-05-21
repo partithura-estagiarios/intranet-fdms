36f386ce9ed28ee67be45324701544cf5a0573dc
oGalhardo <juniorgalhardo03@gmail.com>
2024-05-21 11:28:11 -0300

    🐛 Arrumando style da página home e retirando icon do dataSul

--- src/components/TabHeader/BarUp.vue
+++ src/components/TabHeader/BarUp.vue
@@ -1,49 +1,38 @@
 <template>
-  <q-tabs
-    v-model="tab"
-    class="tab-header text-white ellipsis"
-    :style="{ backgroundColor: tabBackgroundColor }"
-  >
-    <q-item clickable to="/home" class="q-mx-md q-mt-sm">
-      <q-toolbar-title class="q-px-md">
-        <img src="/ico/logo_fundimisa.png" />
-      </q-toolbar-title>
-    </q-item>
+  <q-toolbar class="bg-indigo-8 q-pa-md">
+    <q-btn flat to="/home" class="absolute-top-left">
+      <img src="/ico/logo_fundimisa.png" class="q-pt-md" />
+    </q-btn>
     <q-space />
-    <q-tabs v-model="tab" class="font-tabs bg-transparent text-white q-pa-md">
+    <q-tabs
+      v-model="tab"
+      indicator-color="transparent"
+      class="text-white no-padding no-padding custom-tab-label"
+      active-bg-color="white"
+      shrink
+      active-color="indigo-8"
+    >
       <q-route-tab
-        no-caps
         v-for="item in tabItems"
         :key="item.name"
         :name="item.name"
         :label="$t('tab.' + item.label)"
-        class="text-bold"
-        :class="{ 'text-indigo-8 bg-white rounded-borders': tab === item.name }"
         :to="'/' + item.name"
       />
     </q-tabs>
     <q-space />
-
-    <bt-drop-set />
-  </q-tabs>
+    <div class="row absolute-top-right q-mt-md">
+      <BtDropSet />
+    </div>
+  </q-toolbar>
 </template>
 
 <script setup lang="ts">
 import { tabItems } from "./lib";
 const tab = ref("");
-const tabBackgroundColor = ref("rgba(57, 73, 171, 1)");
-const scrollTop = ref(0);
-function handleScroll() {
-  scrollTop.value = window.pageYOffset;
-  if (!scrollTop.value) {
-    return (tabBackgroundColor.value = "rgba(57, 73, 171, 1)");
-  }
-  return (tabBackgroundColor.value = "rgba(63, 81, 181, 0.8)");
-}
-onMounted(() => {
-  window.addEventListener("scroll", handleScroll);
-});
-watchEffect(() => {
-  handleScroll;
-});
 </script>
+<style scoped>
+.custom-tab-label {
+  line-height: 1vh;
+}
+</style>
--- src/components/TabHeader/ButtonDropdownSettings/BtDropSet.vue
+++ src/components/TabHeader/ButtonDropdownSettings/BtDropSet.vue
@@ -1,35 +1,31 @@
 <template>
-  <q-btn icon="settings" flat size="xl" @click="toggleDropdown">
-    <q-menu>
-      <q-list
-        class="bg-white-8 q-px-none bottom"
-        v-for="(page, index) in item"
-        :key="index"
+  <q-btn-dropdown dropdown-icon="settings" size="1.7vh" flat color="white">
+    <q-list
+      class="bg-white q-px-none bottom"
+      v-for="(page, index) in item"
+      :key="index"
+    >
+      <q-item
+        clickable
+        v-close-popup
+        class="q-px-none"
+        @click="verifyTabs(page.value)"
       >
-        <q-item
-          clickable
-          v-close-popup
-          class="q-px-none"
-          @click="verifyTabs(page.value)"
-        >
-          <q-item-section>
-            <q-item-label class="text-indigo-8">
-              <q-icon v-show="page.name == 'login'" name="power_settings_new" />
+        <q-item-section>
+          <q-item-label class="text-indigo-6">
+            <q-icon v-show="page.name == 'login'" name="power_settings_new" />
 
-              {{ $t("action." + page.name) }}
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-menu>
-  </q-btn>
+            {{ $t("action." + page.name) }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
 </template>
 <script setup lang="ts">
 import { item } from "./lib";
 import { useUsers } from "../../../stores";
 import { router } from "../../../modules";
-
-const on = ref(false);
 
 function verifyTabs(tab: String) {
   if (tab == "logout") {
@@ -37,12 +33,5 @@ function verifyTabs(tab: String) {
     return userStorage.logout();
   }
   return router.push("/" + tab);
-}
-const showList = computed(() => {
-  return on.value;
-});
-
-function toggleDropdown() {
-  on.value = !on.value;
 }
 </script>
