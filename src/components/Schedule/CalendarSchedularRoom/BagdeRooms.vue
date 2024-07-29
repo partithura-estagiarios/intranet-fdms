829c7461be9d9b66d5284aef5436e8fb208b70d4
oGalhardo <juniorgalhardo03@gmail.com>
2024-07-29 14:42:52 -0300

    ♻️ Refatorando codigo,pastas e arquivos do agendador

---
+++ src/components/Schedule/CalendarSchedularRoom/BagdeRooms.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row justify-center font-custom">
+    <div v-for="item in rooms" class="col-auto q-pa-md">
+      <div class="row items-center">
+        <q-badge
+          rounded
+          :style="`background-color:${item.color}`"
+          class="q-mx-sm"
+        />
+        <span class="text-body1 text-black">{{ item.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LoadRooms from "../../../graphql/rooms/LoadRooms.gql";
+
+const rooms = ref();
+async function carregarSalas() {
+  const { loadRooms }: { loadRooms: Object } = await runQuery(LoadRooms);
+  rooms.value = loadRooms;
+}
+onMounted(async () => {
+  carregarSalas();
+});
+</script>
---
+++ src/components/Schedule/CalendarSchedularRoom/ToolbarCalendar.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-toolbar class="row justify-center custom-toolbar">
+    <q-item class="font-custom custom-margin">
+      <q-item-section class="items-center">
+        <q-btn
+          no-caps
+          color="green"
+          :label="$t('text.schedulEvent')"
+          class="text-body1 text-white"
+          @click="$emit('openModalAdd')"
+        />
+      </q-item-section>
+    </q-item>
+    <q-item>
+      <q-item-section class="items-center font-custom">
+        <Month :select-date="props.date" />
+        <NavigationScheduleRoom
+          @today="$emit('today')"
+          @prev="$emit('prev')"
+          @next="$emit('next')"
+        />
+      </q-item-section>
+    </q-item>
+  </q-toolbar>
+</template>
+
+<script setup lang="ts">
+import { useEvents } from "../../../stores/events";
+
+const props = defineProps({
+  date: {
+    type: String,
+    required: true,
+  },
+});
+
+const eventStorage = useEvents();
+const card = ref(false);
+
+const openModalAddScheduleRoom = () => {};
+</script>
+<style scoped>
+.custom-toolbar {
+  margin-top: -8rem;
+}
+.custom-margin {
+  margin-right: 15rem;
+}
+.font-custom {
+  font-family: Fira Sans;
+}
+</style>
