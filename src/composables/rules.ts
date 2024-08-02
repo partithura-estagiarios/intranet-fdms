098ab1e6d0d05a7915cdbcd29b79175fb636f3cf
oGalhardo <juniorgalhardo03@gmail.com>
2024-08-02 10:52:45 -0300

    Corrigindo regra de validação na composable

--- src/composables/rules.ts
+++ src/composables/rules.ts
@@ -8,31 +8,15 @@ export function useFieldValidation() {
   }
   function validateDateTime(val: string): boolean | string {
     if (!val) {
-      return t("auth.fillField");
+      return "Informe Data e Hora";
     }
-
     const dateTimePattern = /^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})$/;
     const match = val.match(dateTimePattern);
     if (!match) {
-      return t("auth.invalidDateTimeFormat");
+      return "Informe a Hora";
     }
-
-    const [_, day, month, year, hour, minute] = match.map(Number);
-    const date = new Date(year, month - 1, day, hour, minute);
-
-    if (
-      date.getFullYear() !== year ||
-      date.getMonth() !== month - 1 ||
-      date.getDate() !== day ||
-      date.getHours() !== hour ||
-      date.getMinutes() !== minute
-    ) {
-      return t("auth.invalidDateTime");
-    }
-
     return true;
   }
-
   return {
     validateNotEmpty: computed(() => validateNotEmpty),
     validateDateTime: computed(() => validateDateTime),
